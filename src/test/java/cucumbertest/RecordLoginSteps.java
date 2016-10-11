8b90c436b36592fa4e1f4a457c79d8f595acb712
¨udr013¨ <udr013@gmail.com>
2016-10-12 01:01:59 +0200

    reformat test using parameters and table, auto close browser after test

--- src/test/java/cucumbertest/RecordLoginSteps.java
+++ src/test/java/cucumbertest/RecordLoginSteps.java
@@ -1,11 +1,14 @@
 package cucumbertest;
 
+import cucumber.api.DataTable;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
 
 /**
  * Created by udr013 on 11-10-16.
@@ -18,12 +21,21 @@ public class RecordLoginSteps {
     public void iMOnMyWebsite() throws Throwable {
         webdriver = new FirefoxDriver();
         webdriver.navigate().to("https://recordcollector.herokuapp.com/");
+        webdriver.manage().window().maximize();
     }
 
-    @When("^i click on collection tab$")
-    public void iClickOnCollectionTab() throws Throwable {
-        webdriver.findElement(By.linkText("Collection")).click();
+//    @When("^i click on collection tab$")
+//    public void iClickOnCollectionTab() throws Throwable {
+//        webdriver.findElement(By.linkText("Collection")).click();
+//    }
+
+    @Then("^i click on \"([^\"]*)\" tab$")
+    public void iClickOnTab(String tab) throws Throwable {
+
+        webdriver.findElement(By.linkText(tab)).click();
+        Thread.sleep(1000);
     }
+
 
     @Then("^i get redirected to login page$")
     public void iGetRedirectedToLoginPage() throws Throwable {
@@ -31,15 +43,29 @@ public class RecordLoginSteps {
     }
 
     @Then("^i fill in my username and password$")
-    public void iFillInMyUsernameAndPassword() throws Throwable {
-        webdriver.findElement(By.name("username")).sendKeys("mark");
-        webdriver.findElement(By.name("password")).sendKeys("test");
+    public void iFillInMyUsernameAndPassword(DataTable table) throws Throwable {
+
+        List<List<String>> data = table.raw();
+//        webdriver.findElement(By.name("username")).sendKeys("mark");
+//        webdriver.findElement(By.name("password")).sendKeys("test");
+
+//            column 1 row 1 / colomn 2 row 1
+        webdriver.findElement(By.name("username")).sendKeys(data.get(1).get(1));
+        webdriver.findElement(By.name("password")).sendKeys(data.get(2).get(1));
         webdriver.findElement(By.className("button")).click();
     }
 
     @Then("^i see my collection$")
     public void iSeeMyCollection() throws Throwable {
+        //Assert.assertEquals("we expect",String);
         webdriver.findElement(By.className("overviewTitle")).getText().equals("Collection overview:");
+        //Assert.assertEquals("we expect",(webdriver.findElement(By.className("overviewTitle")).getText().equals("Collection overview:")==false));
+
+
+    }
+    @And("^i close the browser")
+    public void closeBrowser()  throws Throwable{
+        webdriver.close();
     }
 
 }
--- src/test/resources/cucumbertest/RecordLogin.feature
+++ src/test/resources/cucumbertest/RecordLogin.feature
@@ -3,8 +3,17 @@ Feature: Website should have secured login for collection
 
   Scenario: Check collection page can only accessed when login so you get redirected to login screen
     Given I'm on my website
-    When i click on collection tab
+    Then i click on "About" tab
+    Then i click on "Contact" tab
+    Then i click on "Collection" tab
     Then i get redirected to login page
     And  i fill in my username and password
-    Then i see my collection
+      | fields   | value |
+      | username | mark  |
+      | password | test  |
 
+    Then i see my collection
+    Then i click on "About" tab
+    Then i click on "Contact" tab
+    Then i click on "Logout" tab
+    And i close the browser
